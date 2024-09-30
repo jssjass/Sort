43e1132ff06409b42d3b38273e303c65aa702a69
A <aleksejjss66528@gmail.com>
2024-09-30 15:55:33 +0700

    Итерация 1: добавил заглушки для функций

---
+++ sort.py
@@ -0,0 +1,14 @@
+def generate_random_text(word_count=10):
+    pass
+
+def get_input():
+    pass
+
+def process_text(text):
+    pass
+
+def display_result(sorted_words):
+    pass
+
+def main_menu():
+    pass
